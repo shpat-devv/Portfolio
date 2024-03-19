6623d3e2cb0e3a2c6621da3e737f9e65edb0fa4d
Uglypr1nces <shpatavdiu4@gmail.com>
2024-03-19 12:16:13 +0100

    updated about

--- php/process-form.php
+++ php/process-form.php
@@ -6,12 +6,18 @@ if (isset($_REQUEST['name'], $_REQUEST['email'], $_REQUEST['subject'], $_REQUEST
     $subject = $_REQUEST['subject'];
     $message = $_REQUEST['message'];
 
-    // Set your email address where you want to receive emails.
-    $to = 'unattractivequeen@gmail.com';
+    // Set your Gmail address as the sender.
+    $from = 'unattractivequeen@gmail.com';
 
-    $headers = "From: $name <$mail> \r\n";
+    // Set the recipient email address.
+    $to = 'shpatavdiu4@gmail.com';
 
+    // Set additional headers.
+    $headers = "From: $name <$from> \r\n";
+    $headers .= "Reply-To: $name <$mail> \r\n";
+
+    // Send email.
     $send_email = mail($to, $subject, $message, $headers);
 
     echo ($send_email) ? 'success' : 'error';
-
+}
