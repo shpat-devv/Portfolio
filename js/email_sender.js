cd22fbbcd4b7664a6a4cb579bf3767a550dd752d
Uglypr1nces <shpatavdiu4@gmail.com>
2024-04-23 16:17:04 +0200

    added password protection

---
+++ js/email_sender.js
@@ -0,0 +1,13 @@
+src = "https://smtpjs.com/v3/smtp.js";
+
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
