c4efedf4b068b1551f67c79e589e4b153d7fb502
Uglypr1nces <shpatavdiu4@gmail.com>
2024-04-24 17:17:07 +0200

    added email sending feature

--- js/email_sender.js
+++ js/email_sender.js
@@ -1,13 +1,20 @@
-src = "https://smtpjs.com/v3/smtp.js";
+document.getElementById('sendButton').addEventListener('click', function() {
+  // Get values from input fields
+  var name = document.querySelector('input[name="name"]').value;
+  var email = document.querySelector('input[name="email"]').value;
+  var subject = document.querySelector('input[name="subject"]').value;
+  var message = document.querySelector('textarea[name="message"]').value;
 
-Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "username",
-    Password : "password",
-    To : 'them@website.com',
-    From : "you@isp.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
-}).then(
-  message => alert(message)
-);
+  // Send email using SMTP.js
+  Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "unattractivequeen@gmail.com",
+      Password: "cffs ehnp wlca sxwd",
+      To: 'shpatavdiu4@gmail.com',
+      From: "unattractivequeen@gmail.com",
+      Subject: subject,
+      Body: "Name: " + name + "<br>Email: " + email + "<br>Message: " + message
+  }).then(function(message) {
+      alert(message);
+  });
+});
