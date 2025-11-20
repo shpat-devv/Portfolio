82ed7cc96f0bd9066d2a8596b4ee1b395d5da04f
Shpat Avdiu <uglyprincess@Shpats-MacBook-Pro.local>
2025-11-20 17:49:32 +0100

    fixed popups

--- js/services.js
+++ js/services.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var btn1 = this.getElementById("btn-1");
-    var btn2 = this.getElementById("btn-2");
-    var btn3 = this.getElementById("btn-3");
-    var btn4 = this.getElementById("btn-4");
-
+    var buttons = document.getElementsByClassName("services-button");
+    var btn1 = buttons[0];
+    var mobile_btn1 = buttons[1];
+    var btn2 = buttons[2];
+    var mobile_btn2 = buttons[3];
+    var btn3 = buttons[4];
+    var mobile_btn3 = buttons[5];
+    var btn4 = buttons[6];
+    var mobile_btn4 = buttons[7];
+    
     btn1.addEventListener("click", function() {
+        applicationsPopup.show();
+    });
+
+    mobile_btn1.addEventListener("click", function() {
         applicationsPopup.show();
     });
     
     btn2.addEventListener("click", function() {
+        websitesPopup.show();
+    });
+
+    mobile_btn2.addEventListener("click", function() {
         websitesPopup.show();
     });
     
@@ -16,10 +29,17 @@ document.addEventListener("DOMContentLoaded", function() {
         scriptsPopup.show();
     });
     
+    mobile_btn3.addEventListener("click", function() {
+        scriptsPopup.show();
+    });
+    
     btn4.addEventListener("click", function() {
         itHelpPopup.show();
     });
     
+    mobile_btn4.addEventListener("click", function() {
+        itHelpPopup.show();
+    });
 });
 
 
@@ -92,11 +112,3 @@ const itHelpPopup = new Popup({
     `
 });
 
-
-
-
-
-
-
-
-myPopup.show();
